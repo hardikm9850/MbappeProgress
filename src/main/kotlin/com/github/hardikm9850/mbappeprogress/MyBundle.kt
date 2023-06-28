b3b64a284f3d40a93dfe0743ef5fa57cf6ce9bb6
GitHub Action <action@github.com>
2023-06-28 12:21:24 +0000

    Template cleanup

--- src/main/kotlin/com/github/hardikm9850/mbappeprogress/MyBundle.kt
+++ src/main/kotlin/com/github/hardikm9850/mbappeprogress/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.hardikm9850.mbappeprogress
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/hardikm9850/mbappeprogress/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/hardikm9850/mbappeprogress/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.hardikm9850.mbappeprogress.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
